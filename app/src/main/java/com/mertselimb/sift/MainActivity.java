f636d20204e971de12a059e7a5b0ae7eca9e2d30
Mert Selimbeyoğlu <mertselimb@gmail.com>
2019-06-10 20:00:38 +0300

    init

---
+++ app/src/main/java/com/mertselimb/sift/MainActivity.java
@@ -0,0 +1,45 @@
+package com.mertselimb.sift;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton btnCamera = (FloatingActionButton)findViewById(R.id.takeImg);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        FloatingActionButton takeImg = findViewById(R.id.takeImg);
+        takeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
+}
