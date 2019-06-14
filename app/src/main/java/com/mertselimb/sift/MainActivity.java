0a7078c5963613d00dacd933d993c75f6578fce3
Mert Selimbeyoğlu <mertselimb@gmail.com>
2019-06-15 00:12:23 +0300

    add opencv

--- app/src/main/java/com/mertselimb/sift/MainActivity.java
+++ app/src/main/java/com/mertselimb/sift/MainActivity.java
@@ -1,45 +1,122 @@
 package com.mertselimb.sift;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
-    ImageView imageView;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("opencv_java");
+        System.loadLibrary("nonfree");
+    }
+    ImageView imageToSearch;
+    ImageView imageFound;
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static Uri photoURI;
+    private FeatureDetector detector = FeatureDetector.create(FeatureDetector.SIFT);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton btnCamera = (FloatingActionButton)findViewById(R.id.takeImg);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageToSearch = findViewById(R.id.imageToSearch);
+        imageFound = findViewById(R.id.imageFound);
 
         FloatingActionButton takeImg = findViewById(R.id.takeImg);
         takeImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                dispatchTakePictureIntent();
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+        imageToSearch.setImageURI(photoURI);
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.d40821010_29_9267615_90);
+        //Bitmap bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
+        Mat rgba = new Mat();
+        Utils.bitmapToMat(bmp, rgba);
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        Imgproc.cvtColor(rgba, rgba, Imgproc.COLOR_RGBA2GRAY);
+        detector.detect(rgba, keyPoints);
+        Features2d.drawKeypoints(rgba, keyPoints, rgba);
+        Utils.matToBitmap(rgba, bmp);
+        imageFound.setImageBitmap(bmp);
+
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println(ex.toString());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.mertselimb.sift.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    public MatOfKeyPoint sift(Bitmap inputImage) {
+        Mat rgba = new Mat();
+        Utils.bitmapToMat(inputImage, rgba);
+        MatOfKeyPoint keyPoints = new MatOfKeyPoint();
+        Imgproc.cvtColor(rgba, rgba, Imgproc.COLOR_RGBA2GRAY);
+        detector.detect(rgba, keyPoints);
+        return keyPoints;
     }
 
 }
